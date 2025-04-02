52df79a55af183ef2de857fa070d96e80476f626
Guilherme Recalcatte Vogel <guilhermerecalcatte@gmail.com>
2025-04-02 16:15:20 -0300

    documentar roteador

---
+++ documentar-onu.user.js
@@ -0,0 +1,12 @@
+// ==UserScript==
+// @name         Documentar roteadores huawei
+// @namespace    http://tampermonkey.net/
+// @version      2025-04-02
+// @description  Coleta informações do roteador automaticamente em segundo plano
+// @author       Vogel
+// @downloadURL  https://github.com/gu1zo/scriptsGG/blob/main/documentar-onu.user.js
+// @updateURL    https://github.com/gu1zo/scriptsGG/blob/main/documentar-onu.user.js
+// @match        *://*/html/index.html
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=156.194
+// @grant        GM_setClipboard
+// ==/UserScript==
--- documentar-roteadores.user.js
+++ documentar-roteadores.user.js
@@ -4,8 +4,8 @@
 // @version      2025-04-02
 // @description  Coleta informações do roteador automaticamente em segundo plano
 // @author       Vogel
-// @downloadURL  https://github.com/gu1zo/scriptsGG/blob/main/documentar-roteadores.js
-// @updateURL    https://github.com/gu1zo/scriptsGG/blob/main/documentar-roteadores.js
+// @downloadURL  https://github.com/gu1zo/scriptsGG/blob/main/documentar-roteadores.user.js
+// @updateURL    https://github.com/gu1zo/scriptsGG/blob/main/documentar-roteadores.user.js
 // @match        *://*/html/index.html
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=156.194
 // @grant        GM_setClipboard
