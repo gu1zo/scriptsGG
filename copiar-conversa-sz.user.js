7b838b09a3f537c13810fd3b30ffc65ede18c0af
Guilherme Recalcatte Vogel <guilhermerecalcatte@gmail.com>
2025-04-03 11:36:33 -0300

    copiar Conversa

--- copiar-conversa-sz.user.js
+++ copiar-conversa-sz.user.js
@@ -5,7 +5,7 @@
 // @author       Vogel
 // @downloadURL  https://github.com/gu1zo/scriptsGG/blob/main/copiar-conversa-sz.user
 // @updateURL    https://github.com/gu1zo/scriptsGG/blob/main/copiar-conversa-sz.user
-// @match        https://ggnet.sz.chat/user/agent#
+// @match        *://*.ggnet.sz.chat/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=156.194
 // @grant        GM_setClipboard
 // ==/UserScript==
@@ -16,11 +16,11 @@
     function getXPathValue(xpath) {
         let result = document.evaluate(xpath, document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null);
         if (!result.singleNodeValue) return null;
-        
+
         let img = result.singleNodeValue.querySelector("img");
         return img ? img.src : result.singleNodeValue.textContent.trim();
     }
-    
+
     function expandFullText(liIndex) {
         return new Promise((resolve) => {
             let readMoreXPath = `/html/body/div[1]/main/div[3]/div/div/div/ul/li[${liIndex}]/span/div[1]/div/div[1]/div[2]/div/div[1]/a`;
@@ -33,48 +33,46 @@
             }
         });
     }
-    
+
     async function processListItems() {
         let listItems = document.querySelectorAll("#list_mensagens > ul > li");
         console.log("Total de LIs:", listItems.length);
-        
+
         let resultString = "";
-        
+
         for (let index = 0; index < listItems.length; index++) {
             let liIndex = index + 1;
             await expandFullText(liIndex);
-            
+
             let xpath1 = `/html/body/div[1]/main/div[3]/div/div/div/ul/li[${liIndex}]/span/div[1]/div/div[1]/div[1]/div[1]/div`;
             let xpath2 = `/html/body/div[1]/main/div[3]/div/div/div/ul/li[${liIndex}]/span/div[1]/div/div[1]/div[2]/div/div[1]/span`;
-            
+
             let value1 = getXPathValue(xpath1) || "";
             let value2 = getXPathValue(xpath2) || "";
-            
+
             resultString += `${value1}\n${value2}\n\n`;
         }
-        
+
         GM_setClipboard(resultString.trim());
         alert("Conversas copiadas para a área de transferência!");
     }
 
-    function createCopyButton() {
-        let button = document.createElement("button");
-        button.innerText = "Copiar Conversas";
-        button.style.position = "fixed";
-        button.style.bottom = "20px";
-        button.style.right = "20px";
-        button.style.zIndex = "1000";
-        button.style.padding = "10px 15px";
-        button.style.background = "#007bff";
-        button.style.color = "#fff";
-        button.style.border = "none";
-        button.style.borderRadius = "5px";
+    function addCopyButton() {
+        let container = document.querySelector(".tags-sessions");
+        if (!container) return;
+
+        let menu = container.querySelector(".menu.tags.ft-scroll");
+        if (!menu) return;
+
+        let button = document.createElement("a");
+        button.setAttribute("data-v-5cbb963e", "");
+        button.className = "item text-ellipsis";
+        button.innerHTML = '<i data-v-5cbb963e class="icon tag"></i> Copiar Conversa';
         button.style.cursor = "pointer";
-        
-        button.onclick = processListItems;
-        
-        document.body.appendChild(button);
+        button.addEventListener("click", processListItems);
+
+        menu.insertBefore(button, null); // Adiciona o botão como último filho da div
     }
 
-    createCopyButton();
+    addCopyButton();
 })();
